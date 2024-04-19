9438ec0f52e708f0f3de43960de484d803cd98a7
Kirusanth <it21838170@my.sliit.lk>
2024-04-19 14:47:36 +0530

    serverjs updated with the routes

--- enrollment-service/server.js
+++ enrollment-service/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const enrollmentRoutes = require('./routes/enrollmentRoutes'); // Assuming routes are in a 'routes' directory
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
+// Use the enrollment routes for requests to /enrollments
+app.use('/enrollments', enrollmentRoutes);
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
-}
-);
+});
 
 app.listen(port, () => {
     console.log(`Enrollment service listening at http://localhost:${port}`);
-}
-);
+});
