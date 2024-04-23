858551737f52260fafa89ea3d0c287af397b027c
Kirusanth <it21838170@my.sliit.lk>
2024-04-23 16:43:22 +0530

    Remove enrollment-service.yaml and update server.js

--- enrollment-service/server.js
+++ enrollment-service/server.js
@@ -16,10 +16,6 @@ app.use(bodyParser.json());
 // Use the enrollment routes for requests to /enrollments
 app.use('/enrollments', enrollmentRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
 app.listen(port, () => {
     console.log(`Enrollment service listening at http://localhost:${port}`);
 });
