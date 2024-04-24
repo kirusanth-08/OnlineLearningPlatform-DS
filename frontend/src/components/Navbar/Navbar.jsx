d5754421ab5616768d3ceda35ff892023d17937c
Kirusanth <it21838170@my.sliit.lk>
2024-04-24 07:00:04 +0530

    Refactor Navbar component styling and add search functionality

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,27 +1,43 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState } from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [user, setUser] = useState(null); // Replace this with your actual user state/context
+
   return (
     <div className='navbar'>
-        <div className='navbar__main'>
-            <div className='navbar__logo'>
+        <div className='navbar-main'>
+            <div className='navbar-main__logo'>
                 <a href="#">Alright</a>
             </div>
-            <div className='navbar__links'>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
+            <div className='navbar-main__search'>
+                <input type="text" placeholder="Search for courses" />
+                <button>Search</button>
             </div>
-            <div className='navbar__cta'>
-                <a href="#" className='btn'>Get Started</a>
+            <div className='navbar-main__links'>
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Services</a>
+                    <a href="#">Contact</a>
             </div>
-        </div>    
+            <div className='navbar-main__cta'>
+                {user ? (
+                    <a href="#" className='btn'>Profile</a>
+                ) : (
+                    <a href="#" className='btn'>Get Started</a>
+                )}
+            </div>
+        </div>
+        <div className='navbar-user'>
+                <div className='navbar-user__links'>
+                    <a href="#">My Learnings</a>
+                    <a href="#">Settings</a>
+                    <a href="#">Logout</a>
+                </div>
+            
+        </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
