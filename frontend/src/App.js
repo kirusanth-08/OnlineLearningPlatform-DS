43e46a2b64a62b1c132519703aff1ec2211c7202
Kirusanth <it21838170@my.sliit.lk>
2024-04-26 06:31:02 +0530

    Refactor file structure and update dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import ContinueLearning from './components/ContinueLearning/ContinueLearning';
+import Navbar from './Learner/components/Navbar/Navbar';
+import ContinueLearning from './Learner/components/ContinueLearning/ContinueLearning';
 import Dashboard from './Instructor/pages/Dashboard';
 
 function App() {
