1f610ca004cdfb0f1123bc14347e52cb6cbe5854
Kirusanth <it21838170@my.sliit.lk>
2024-04-29 08:10:26 +0530

    Refactor code and remove unnecessary files

--- User_Service/routes/instructor.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const instructorCont = require('../controllers/instructorController')
-const verify = require('../middleware/verfyToken')
-
-router.get('/:id',verify,instructorCont.viewProfile) 
-router.put('/update/:id',verify,instructorCont.updateProfile) 
-
-module.exports = router
--- User_Service/routes/student.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const studentController = require('../controllers/studentController')
-const verify = require('../middleware/verfyToken')
-
-router.get('/:id',verify,studentController.viewProfile)
-router.put('/update/:id',verify,studentController.updateProfile)
-
-module.exports = router
